a7810a9ae70e7d3aed07049e17ae732310f361ab
HughesSeanKyle <khughessean@yahoo.com>
2020-10-27 14:54:02 +0200

    V36 - Passing props solution - slight refactor

--- src/index.js
+++ src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" avatar={faker.image.avatar()} comment="React is really interesting" />
-            <CommentDetail author="Alex" timeAgo="Today at 2:00AM" avatar={faker.image.avatar()} comment="Yeah, I agree. " />
-            <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" avatar={faker.image.avatar()} comment="So awesome!" />
+            <CommentDetail 
+              author="Sam" 
+              timeAgo="Today at 4:45PM" 
+              avatar={faker.image.avatar()} 
+              comment="React is really interesting" 
+            />
+            <CommentDetail 
+              author="Alex" 
+              timeAgo="Today at 2:00AM" 
+              avatar={faker.image.avatar()} 
+              comment="Yeah, I agree. " 
+            />
+            <CommentDetail 
+              author="Jane" 
+              timeAgo="Yesterday at 5:00PM" 
+              avatar={faker.image.avatar()} 
+              comment="So awesome!" 
+            />
         </div>
     ); 
 ;}
